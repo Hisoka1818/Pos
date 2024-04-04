9be14ac2115d82da4003f90485431967315c7038
DESKTOP-H28OU24\Camus <jhon14fredygomez@gmail.com>
2024-04-03 21:25:58 -0500

    DataContext

---
+++ Pos.Web/CustomConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pos.Web.Data;
+
+namespace Pos.Web
+{
+    public static class CustomConfiguration
+    {
+        public static WebApplicationBuilder AddCustomBuilderConfiguration(this WebApplicationBuilder builder)
+        {
+            // Data Context
+            builder.Services.AddDbContext<DataContext>(conf =>
+            {
+                conf.UseSqlServer(builder.Configuration.GetConnectionString("MyConnection"));
+            });
+
+            return builder;
+        }
+    }
+}
